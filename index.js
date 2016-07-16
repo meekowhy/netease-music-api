d9d3af2d9e8eaf3a4fcd921adc1443da7dac16d7
syaning <syaningv@gmail.com>
2016-07-16 15:48:18 +0800

    add artist

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const album = require('./lib/album')
+const artist = require('./lib/artist')
 const playlist = require('./lib/playlist')
 const song = require('./lib/song')
 
 module.exports = {
   album,
+  artist,
   playlist,
   song
 }
